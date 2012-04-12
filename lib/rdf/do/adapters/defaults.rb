1c441e94cb9d28ce9cf85bf318efbdf522d593ff
grandison <grandison@mail.ru>
2012-04-12 19:44:25 +0400

    Update lib/rdf/do/adapters/defaults.rb

--- lib/rdf/do/adapters/defaults.rb
+++ lib/rdf/do/adapters/defaults.rb
@@ -53,12 +53,26 @@ module RDF::DataObjects
         params = []
         [:subject, :predicate, :object, :context].each do |resource|
           unless hash[resource].nil?
-            conditions << "#{resource.to_s} = ?"
+            if resource == :context
+              case hash[resource]
+              when Symbol, RDF::Query::Variable
+                conditions << "#{resource.to_s} != 'nil'"
+                next
+              when false
+                conditions << "#{resource.to_s} = 'nil'"
+                next
+              else
+                conditions << "#{resource.to_s} = ?"
+              end
+            else
+              conditions << "#{resource.to_s} = ?"
+            end
             params     << repository.serialize(hash[resource])
           end
         end
         where = conditions.empty? ? "" : "WHERE "
         where << conditions.join(' AND ')
+        puts "query: #{where.inspect}, #{params.inspect}"
         repository.result('select * from quads ' + where, *params)
       end
 
